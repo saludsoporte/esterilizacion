7ade64564b7dd7b97312e5b8a9be99999afd680a
salud.aplicaciones@gmail.com <salud.aplicaciones@gmail.com>
2026-08-24 16:03:07 -0600

    .

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AgendaController from "./agenda_controller"
+application.register("agenda", AgendaController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
